2bf963f61accff929f11825b7d1ef3dc9ca0483a
Bruno Petrus <bruno.petrus@live.com>
2018-06-10 23:51:32 +0200

    Implemented not so ideal calculateFPS function.

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,40 @@
+// Graphics
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+// Utilities
 #include <iostream>
+#include <chrono>
 
 #include "RectanglePlayer.hpp"
+
+void calculateFPS();
 
 const int WINDOW_HEIGH = 800;
 const int WINDOW_WIDTH = 800;
 
-int main() {
+int main()
+{
 
     // Create the window
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGH), "Simple Movement System");
-    window.setFramerateLimit(30);
+    window.setFramerateLimit(60);
 
-    RectanglePlayer player(sf::Vector2f(50,50), sf::Vector2f(100, 100), sf::Color::Yellow);
+    // Initialize timer
+
+    RectanglePlayer player(sf::Vector2f(50, 50), sf::Vector2f(100, 100), sf::Color::Yellow);
 
     // Run the program
-    while(window.isOpen()) {
+    while (window.isOpen())
+    {
+        calculateFPS();
+
         // Events
         sf::Event event;
-        while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
             }
         }
@@ -35,4 +48,26 @@ int main() {
     }
 
     return 0;
+}
+
+/**
+ * \brief Calculates and prints out FPS
+ * 
+ * Keeps track of how many frames passed over a set interval and calculates FPS, which is then printed out using std::cout.
+ */
+void calculateFPS()
+{
+    static std::chrono::time_point<std::chrono::high_resolution_clock> start = std::chrono::high_resolution_clock::now();
+    static int frames = 1;
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> end = std::chrono::high_resolution_clock::now();
+    frames++;
+
+    auto deltaTime = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - start).count();
+    if (deltaTime > 1000)
+    {
+        std::cout << "FPS: " << frames / (deltaTime / 1000) << std::endl;
+        start = end;
+        frames = 0;
+    }
 }
